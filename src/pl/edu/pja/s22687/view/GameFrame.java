d84d042628cfb8ef9eb5168daf7c3d832cf539b3
Przemysław Ren <przemyslaw.ren@gmail.com>
2023-11-28 20:13:08 +0100

    change in GameFrame

--- src/pl/edu/pja/s22687/view/GameFrame.java
+++ src/pl/edu/pja/s22687/view/GameFrame.java
@@ -2,18 +2,18 @@ package pl.edu.pja.s22687.view;
 
 import javax.swing.*;
 
-public class GameFrame {
+public class GameFrame extends JFrame {
 
 
-    public GameFrame(int x, int y) {
-        JFrame frame = new JFrame("Pacman");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
-        frame.setLocationRelativeTo(null);
-
-        JTable table = new JTable(x, y);
-
-        frame.add(table);
-        frame.setVisible(true);
+    public GameFrame(int rows, int columns) {
+        super("Pacman");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultLookAndFeelDecorated(true);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setIconImage(new ImageIcon("src/pl/edu/pja/s22687/resources/ghost.png").getImage());
+        JTable table = new JTable(rows, columns);
+        add(table);
+        setVisible(true);
     }
 }
